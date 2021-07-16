997df4e4637e498fc9ff19e3da5fb8ffa94d8106
Trabelsi Tobias <tobias.trabelsi@sonarsource.com>
2021-07-16 14:19:08 +0200

    Review feedback

--- .cirrus/append_build_number.sh
+++ .cirrus/append_build_number.sh
@@ -5,6 +5,6 @@ FILES="charts/*/Chart.yaml"
 for f in $FILES
 do
   echo "Updateing version of $f to append $BUILD_NUMBER"
-  _original_version=`cat $f | yq r - version `
+  _original_version=${cat $f | yq r - version }
    yq w -i $f version "$_original_version+${BUILD_NUMBER}"
 done
--- .cirrus/upload_chart.sh
+++ .cirrus/upload_chart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-CURRENT_DIR=`pwd`
+CURRENT_DIR=${pwd}
 
-for chart in `find $CIRRUS_WORKING_DIR -name "*.tgz" -type f -exec basename "{}" ";"`; do
+for chart in ${find $CIRRUS_WORKING_DIR -name "*.tgz" -type f -exec basename "{}" ";"}; do
     cd $CIRRUS_WORKING_DIR
     echo "Uploading $chart"
     curl "-u${ARTIFACTORY_DEPLOY_USERNAME}:${ARTIFACTORY_DEPLOY_PASSWORD}" "-T $chart" "https://repox.jfrog.io/artifactory/sonarsource-helm-builds/$chart"
