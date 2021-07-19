ff0d2d9e0e1b9e3d5d2c96380f0ef6575bd6836f
Tobias Trabelsi <64127335+tobias-trabelsi-sonarsource@users.noreply.github.com>
2021-07-19 10:36:13 +0200

    BUILD-1095 push artifact to repox
    * BUILD-1095 uploading package to artifactory
    
    * BUILD-1095 switching to busybox find syntax
    
    * BUILD-1095 replaced working dir
    
    * BUILD-1095 search only in the current dir
    
    * BUILD-1095 fixed wrong brackets
    
    * BUILD-1095 cache env seems funny
    
    * BUILD-1095 fail fast
    
    * BUILD-1095 review feedback

--- .cirrus/append_build_number.sh
+++ .cirrus/append_build_number.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+set -euo pipefail
+
 FILES="charts/*/Chart.yaml"
 
 for f in $FILES
 do
   echo "Updateing version of $f to append $BUILD_NUMBER"
-  _original_version=`cat $f | yq r - version `
+  _original_version=$(cat $f | yq r - version)
    yq w -i $f version "$_original_version+${BUILD_NUMBER}"
 done
