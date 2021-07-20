f934295f0a6f2e5331fdf602b1087abd83e48f1b
Trabelsi Tobias <tobias.trabelsi@sonarsource.com>
2021-07-20 08:53:10 +0200

    BUILD-1094 sign with gpg

--- .cirrus/sign_chart.sh
+++ .cirrus/sign_chart.sh
@@ -6,6 +6,6 @@ CURRENT_DIR=$(pwd)
 
 for chart in $(find $CIRRUS_WORKING_DIR -maxdepth 1 -name "*.tgz*" -type f -exec basename "{}" ";"); do
     cd $CIRRUS_WORKING_DIR
-    echo ${SONARSOURCE_SIGN_KEY_PASSPHRASE} | helm-sign --keyring ~/signing-keyring $chart
+    echo ${SONARSOURCE_SIGN_KEY_PASSPHRASE} | gpg --batch --yes --passphrase-fd 0 --output $chart.sig --detach-sig $chart
     cd $CURRENT_DIR
 done
