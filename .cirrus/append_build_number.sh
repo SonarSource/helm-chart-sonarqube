738026f1314c67973e1338c38b26d15d3a074641
Trabelsi Tobias <tobias.trabelsi@sonarsource.com>
2021-07-20 15:58:39 +0200

    BUILD-1087 switch versioning depending on release env being set

--- .cirrus/append_build_number.sh
+++ .cirrus/append_build_number.sh
@@ -8,9 +8,9 @@ for f in $FILES
 do
   echo "Updateing version of $f to append $BUILD_NUMBER"
   _original_version=$(cat $f | yq r - version)
-  if [ "$CIRRUS_BRANCH" = "master" ]; then
+  if [[ -v CIRRUS_RELEASE ]]; then
     yq w -i $f version "$_original_version+${BUILD_NUMBER}"
   else
     yq w -i $f version "$_original_version-${BUILD_NUMBER}"
-  fi   
+  fi
 done
