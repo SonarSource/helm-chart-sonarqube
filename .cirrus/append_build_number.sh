5d28d0323e036f01682c573c0db806d4e4ac4523
Trabelsi Tobias <tobias.trabelsi@sonarsource.com>
2021-07-20 14:50:13 +0200

    BUILD-1087 always build pre releases

--- .cirrus/append_build_number.sh
+++ .cirrus/append_build_number.sh
@@ -8,5 +8,5 @@ for f in $FILES
 do
   echo "Updateing version of $f to append $BUILD_NUMBER"
   _original_version=$(cat $f | yq r - version)
-   yq w -i $f version "$_original_version+${BUILD_NUMBER}"
+   yq w -i $f version "$_original_version-${BUILD_NUMBER}"
 done
