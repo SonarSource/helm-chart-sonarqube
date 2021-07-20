9845f7b79950fad79aa18e7a754edd4ea25ab76a
Trabelsi Tobias <tobias.trabelsi@sonarsource.com>
2021-07-20 09:55:12 +0200

    BUILD-1094 temporarely deactivated testing and import key for signing

--- .cirrus/sign_chart.sh
+++ .cirrus/sign_chart.sh
@@ -2,10 +2,13 @@
 
 set -euo pipefail
 
+echo $SONARSOURCE_SIGN_KEY > /tmp/key
+echo $SONARSOURCE_SIGN_KEY_PASSPHRASE | gpg --batch --yes --passphrase-fd 0 --import /tmp/key
+
 CURRENT_DIR=$(pwd)
 
 for chart in $(find $CIRRUS_WORKING_DIR -maxdepth 1 -name "*.tgz*" -type f -exec basename "{}" ";"); do
     cd $CIRRUS_WORKING_DIR
-    echo ${SONARSOURCE_SIGN_KEY_PASSPHRASE} | gpg --batch --yes --passphrase-fd 0 --output $chart.sig --detach-sig $chart
+    echo $SONARSOURCE_SIGN_KEY_PASSPHRASE | gpg --batch --yes --passphrase-fd 0 --output $chart.sig --detach-sig $chart
     cd $CURRENT_DIR
 done
