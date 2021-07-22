9e4b78d4571cedef621bce6d9555ef5b14d7bdbf
Trabelsi Tobias <tobias.trabelsi@sonarsource.com>
2021-07-22 13:42:12 +0200

    BUILD-1087 using jfrog cli to upload chart with build number attached

--- .cirrus/upload_chart.sh
+++ .cirrus/upload_chart.sh
@@ -4,17 +4,12 @@ set -euo pipefail
 
 CURRENT_DIR=$(pwd)
 
+jfrog config add repox --artifactory-url ${ARTIFACTORY_URL} --user ${ARTIFACTORY_DEPLOY_USERNAME} --password ${ARTIFACTORY_DEPLOY_PASSWORD} --basic-auth-only
+
 for chart in $(find $CIRRUS_WORKING_DIR -maxdepth 1 -name "*.tgz*" -type f -exec basename "{}" ";"); do
     cd $CIRRUS_WORKING_DIR
-    _ARTIFACT_MD5_CHECKSUM=$(md5sum $chart | awk '{ print $1 }')
-    _ARTIFACT_SHA1_CHECKSUM=$(shasum -a 1 $chart | awk '{ print $1 }')
-    _ARTIFACT_SHA256_CHECKSUM=$(sha256sum $chart | awk '{ print $1 }')
-    echo "Uploading $chart"
-    curl "-u${ARTIFACTORY_DEPLOY_USERNAME}:${ARTIFACTORY_DEPLOY_PASSWORD}" \
-        -T "$chart" \
-        -H "X-Checksum-MD5:${_ARTIFACT_MD5_CHECKSUM}" \
-        -H "X-Checksum-Sha1:${_ARTIFACT_SHA1_CHECKSUM}" \
-        -H "X-Checksum-Sha256:${_ARTIFACT_SHA256_CHECKSUM}" \
-        "https://repox.jfrog.io/artifactory/sonarsource-helm-builds/$chart"
+    jfrog rt upload --build-name "sonarqube-helm" --build-number "$BUILD_NUMBER" "$chart" sonarsource-helm-builds
     cd $CURRENT_DIR
 done
+
+jfrog rt build-publish "sonarqube-helm" "$BUILD_NUMBER"
