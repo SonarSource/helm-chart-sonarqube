014b2b376079b3b8010580a486ff48f4fb191622
jeremy cotineau <jeremy.cotineau@sonarsource.com>
2023-03-31 10:39:45 +0200

    SQDEVOPS-96 scan project on next

---
+++ .cirrus/generate-yaml-for-next-scan.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+KUBE_VERSION="${KUBE_VERSION:-1.25.0}"
+STATIC_TEST_FOLDER='../../tests/unit-compatibility-test/'$(basename $PWD)
+YAML_SCAN_FOLDER='../../helm-chart-yaml-files/'$(basename $PWD)
+
+rm -R $YAML_SCAN_FOLDER 2>/dev/null || true
+mkdir -p $YAML_SCAN_FOLDER
+
+
+if ! [ -d "$STATIC_TEST_FOLDER" ]; then
+    echo "$STATIC_TEST_FOLDER folder not found"
+    echo "you are probably not in the root of a chart"
+    exit 1
+fi
+
+echo 'Running templating of our helm chart for scanning them on next'
+
+for file in "$STATIC_TEST_FOLDER"/*; do
+    TEST_CASE_NAME=$(basename "$file")
+    echo 'Entering templating for' $TEST_CASE_NAME
+    helm template --kube-version $KUBE_VERSION --dry-run --debug --set postgresql.enabled=false -f "$file" $TEST_CASE_NAME . > $YAML_SCAN_FOLDER/$TEST_CASE_NAME
+    echo 'Ending templating for' $TEST_CASE_NAME
+done
