fc896f969b4eb395bdc2d7c01418225398df31d4
jeremy cotineau <jeremy.cotineau@sonarsource.com>
2023-11-09 11:26:30 +0100

    SONAR-20657 change wait for kind behavior
    Add a retry mechanism at creation time, this should prevent flakyness

--- .cirrus/wait_for_kind.sh
+++ .cirrus/wait_for_kind.sh
@@ -5,27 +5,23 @@
 # That is why the timeout is set to 25 minutes (1500 seconds)
 timeout=1500
 
-# Set the interval in seconds to check for the cluster
-interval=5
+# Set the interval in seconds to wait for the cluster to be up
+interval=200
 
 echo "Waiting for Kind cluster"
 
 # Loop until the timeout is reached or the cluster is available
 for ((i=0; i<$timeout; i+=$interval)); do
-  # Check if the Kind cluster is available, by checking if the cluster-info error result is empty
-  if [[ -z "$(kubectl cluster-info --context kind-kind 2>&1 > /dev/null)" ]]; then
+  # Check if the Kind cluster is available, by checking the return code.
+  if kind create cluster --wait "${interval}"s -v 8; then
     echo "Kind cluster is available!"
     exit 0
+  else
+    echo "kind cluster creation failed, retrying"
+    kind delete cluster > /dev/null 2>&1
   fi
-
-  # Wait for the interval before checking again
-  sleep "${interval}"
 done
 
 # Timeout reached, Kind cluster is not available
-echo "Timeout reached, Kind cluster is not available"
-kubectl cluster-info --context kind-kind 2>&1
-echo "-------------------------"
-echo "Detailed logs:"
-kubectl cluster-info dump --context kind-kind 2>&1
+echo "Timeout reached, Kind cluster is not available, exiting. Please read the logs on the above trace"
 exit 1
