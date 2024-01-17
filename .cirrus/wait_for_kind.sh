f4722722632746115a56fc28e48e6736b8e81668
Jérémy Cotineau <jeremy.cotineau@sonarsource.com>
2024-01-17 12:03:44 +0100

    SONAR-21408 run static test in PR (#410)
    * SONAR-21408 run static test in PR

--- .cirrus/wait_for_kind.sh
+++ .cirrus/wait_for_kind.sh
@@ -8,12 +8,15 @@ timeout=1500
 # Set the interval in seconds to wait for the cluster to be up
 interval=200
 
+# Set the default Kubernetes version to use
+KUBE_VERSION="${KUBE_VERSION:-1.25.0}"
+
 echo "Waiting for Kind cluster"
 
 # Loop until the timeout is reached or the cluster is available
 for ((i=0; i<$timeout; i+=$interval)); do
   # Check if the Kind cluster is available, by checking the return code.
-  if kind create cluster --wait "${interval}"s -v 8; then
+  if kind create cluster --image "kindest/node:v${KUBE_VERSION}" --wait "${interval}"s -v 8; then
     echo "Kind cluster is available!"
     exit 0
   else
