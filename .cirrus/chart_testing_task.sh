cd42a789e0223ac8c53788b90fd28d2ee0e64206
Davi Koscianski Vidal <davi.koscianski-vidal@sonarsource.com>
2024-01-19 15:56:25 +0100

    SONAR-21430 - Allows external PRs to be built

---
+++ .cirrus/chart_testing_task.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [[ -n "${DOCKER_USERNAME}" ]] && [[ -n "${DOCKER_PASSWORD}" ]]; then
+    helm repo add clustersecret https://charts.clustersecret.io/
+    helm install clustersecret clustersecret/cluster-secret -n clustersecret --create-namespace
+    DOCKER_CONFIG=$(kubectl create secret docker-registry unused --docker-username=${DOCKER_USERNAME} --docker-password=${DOCKER_PASSWORD} --dry-run -o json | jq '.data.".dockerconfigjson"')
+    sed -i "s|DOCKER_CONFIG_JSON|${DOCKER_CONFIG}|g" .cirrus/docker_hub_test_pull_secret.yaml
+    kubectl apply -f .cirrus/docker_hub_test_pull_secret.yaml
+fi
+
+ct lint --config test.yaml --all
+ct install --config test.yaml --all
