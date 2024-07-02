be2281a6cec2201593888f70401b471a6538d1cb
Davi Koscianski Vidal <davi.koscianski-vidal@sonarsource.com>
2024-07-02 10:50:57 +0200

    SONAR-22465 Skips package pushing on PRs

--- .cirrus/package.sh
+++ .cirrus/package.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-set -xeo pipefail
+set -xeuo pipefail
+
+: "${CIRRUS_BRANCH:?}"
+: "${BUILD_NUMBER:?}"
+: "${CIRRUS_BASE_BRANCH:=}"
 
 VERSION_SEPERATOR="-"
 [[ ${CIRRUS_RELEASE:-} != "" ]] && VERSION_SEPERATOR="+"
 
-PREVIOUS_RELEASE=$(gh api "/repos/{owner}/{repo}/releases" --jq "[.[] | select(.target_commitish==\"${CIRRUS_BRANCH}\")][1].tag_name")
+[[ -n "${CIRRUS_BASE_BRANCH}" ]] && TARGET_BRANCH="${CIRRUS_BASE_BRANCH}" || TARGET_BRANCH="${CIRRUS_BRANCH}"
+
+PREVIOUS_RELEASE=$(gh api "/repos/{owner}/{repo}/releases" --jq "[.[] | select(.target_commitish==\"${TARGET_BRANCH}\")][1].tag_name")
 
 # There MIGHT be a some edge case where PREVIOUS_RELEASE shouldn't be HEAD,
 # for example, releasing a patch for non-LTA. To be investigated.
 [[ -z "${PREVIOUS_RELEASE}" ]] && PREVIOUS_RELEASE="HEAD" || echo "${PREVIOUS_RELEASE}"
 
-echo $(ct list-changed --since "${PREVIOUS_RELEASE}" --target-branch "${CIRRUS_BRANCH}")
+CHARTS=$(ct list-changed --since "${PREVIOUS_RELEASE}" --target-branch "${TARGET_BRANCH}")
 
-for chart in $(ct list-changed --since "${PREVIOUS_RELEASE}" --target-branch "${CIRRUS_BRANCH}"); do
-    _original_version=$(cat $chart/Chart.yaml | yq '.version' -)
+echo "${CHARTS}"
+
+for chart in ${CHARTS}; do
+    _original_version=$(yq '.version' "${chart}"/Chart.yaml)
     _new_version="${_original_version}${VERSION_SEPERATOR}${BUILD_NUMBER}"
     helm dependency build "${chart}"
     helm package --version "${_new_version}" "${chart}"
