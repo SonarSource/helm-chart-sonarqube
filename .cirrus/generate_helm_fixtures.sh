e56a6ec72a92246e61adc81a3557ef1d7d3977a1
jeremy cotineau <jeremy.cotineau@sonarsource.com>
2024-07-15 17:01:02 +0200

    SONAR-22530 implement simple fixtures

---
+++ .cirrus/generate_helm_fixtures.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euo pipefail
+
+KUBE_VERSION="${KUBE_VERSION:-1.25.0}"
+
+echo 'Running fixtures'
+
+for path in "sonarqube" "sonarqube-dce"; do
+    STATIC_TEST_FOLDER="./tests/unit-compatibility-test/${path}"
+    CHART_TEST_FOLDER="./charts/${path}"
+
+    if ! [ -d "$STATIC_TEST_FOLDER" ]; then
+        echo "$STATIC_TEST_FOLDER folder not found"
+        echo "${path} not a valid chart path"
+        exit 1
+    fi
+
+    echo "Processing chart: $path"
+
+    for file in "${STATIC_TEST_FOLDER}"/*; do
+        TEST_CASE_NAME=$(basename "$file")
+        FIXTURE_STATIC_TEST_FOLDER="./tests/unit-compatibility-test/fixtures/${path}/${TEST_CASE_NAME}"
+
+        echo "Entering test for ${TEST_CASE_NAME}"
+        helm template --set global.postgresql.postgresqlPostgresPassword='toto' --kube-version "$KUBE_VERSION" --dry-run --debug -f "$file" "${TEST_CASE_NAME}" ${CHART_TEST_FOLDER} > "${FIXTURE_STATIC_TEST_FOLDER}"
+        echo "Ending test for ${TEST_CASE_NAME}"
+    done
+
+done
