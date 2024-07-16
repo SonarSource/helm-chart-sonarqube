d3fcab7f566a215ecdfca5a31d6e2fa41cf33a6a
jeremy cotineau <jeremy.cotineau@sonarsource.com>
2024-07-16 14:07:29 +0200

    SONAR-22530 rephrase debug logs

--- .cirrus/generate_helm_fixtures.sh
+++ .cirrus/generate_helm_fixtures.sh
@@ -15,15 +15,15 @@ for path in "sonarqube" "sonarqube-dce"; do
         exit 1
     fi
 
-    echo "Processing chart: $path"
+    echo "Processing fixtures for chart: $path"
 
     for file in "${STATIC_TEST_FOLDER}"/*; do
         TEST_CASE_NAME=$(basename "$file")
         FIXTURE_STATIC_TEST_FOLDER="./tests/unit-compatibility-test/fixtures/${path}/${TEST_CASE_NAME}"
 
-        echo "Entering test for ${TEST_CASE_NAME}"
+        echo "Entering fixture test for ${TEST_CASE_NAME}"
         helm template --set global.postgresql.postgresqlPostgresPassword='toto' --kube-version "$KUBE_VERSION" --dry-run --debug -f "$file" "${TEST_CASE_NAME}" ${CHART_TEST_FOLDER} > "${FIXTURE_STATIC_TEST_FOLDER}"
-        echo "Ending test for ${TEST_CASE_NAME}"
+        echo "Ending fixture test test for ${TEST_CASE_NAME}"
     done
 
 done
