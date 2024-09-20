926be9da459dced07a8ab89ad9b2349c000758ff
Davi Koscianski Vidal <davi.koscianski-vidal@sonarsource.com>
2024-09-20 12:15:08 +0200

    Refactors .cirrus/unit_helm_compatibility_test.sh

--- .cirrus/unit_helm_compatibility_test.sh
+++ .cirrus/unit_helm_compatibility_test.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
+
 set -euo pipefail
 
+PROJECT_ROOT=$(readlink -f "$(dirname "$0")"/..)
+CHART_NAME="${1:-$(basename "${PWD}")}"
+
+# NOTE: CHART_PATH is set to the first argument if it is provided,
+# otherwise it is set to "." (current dir)
+CHART_PATH="${1:+charts/$1}"
+CHART_PATH="${CHART_PATH:-.}"
+
 KUBE_VERSION="${KUBE_VERSION:-1.25.0}"
-STATIC_TEST_FOLDER='../../tests/unit-compatibility-test/'$(basename $PWD)
+STATIC_TEST_FOLDER="${PROJECT_ROOT}/tests/unit-compatibility-test/${CHART_NAME}"
 
-
-if ! [ -d "$STATIC_TEST_FOLDER" ]; then
-    echo "$STATIC_TEST_FOLDER folder not found"
+if ! [[ -d "${STATIC_TEST_FOLDER}" ]]; then
+    echo "${STATIC_TEST_FOLDER} folder not found"
     echo "you are probably not in the root of a chart"
     exit 1
 fi
 
-echo 'Running unit compatibility tests for Kubernetes version' $KUBE_VERSION
+echo "Running unit compatibility tests for Kubernetes version ${KUBE_VERSION}"
 
-for file in "$STATIC_TEST_FOLDER"/*; do
-    TEST_CASE_NAME=$(basename "$file")
-    echo 'Entering test for' $TEST_CASE_NAME
-    helm template --kube-version $KUBE_VERSION --dry-run --debug -f "$file" $TEST_CASE_NAME . | kubeconform --kubernetes-version $KUBE_VERSION --summary --strict -schema-location default -schema-location "../../tests/test-crds/{{.ResourceKind}}.json"
-    echo 'Ending test for' $TEST_CASE_NAME
+for file in "${STATIC_TEST_FOLDER}"/*; do
+    TEST_CASE_NAME=$(basename "${file}")
+    echo "Entering test for ${TEST_CASE_NAME}"
+    helm template \
+        --kube-version "${KUBE_VERSION}" \
+        --dry-run \
+        --debug \
+        -f "${file}" "${TEST_CASE_NAME}" "${CHART_PATH}" \
+    | kubeconform \
+        --kubernetes-version "${KUBE_VERSION}" \
+        --summary \
+        --strict -schema-location default \
+        -schema-location "${PROJECT_ROOT}/tests/test-crds/{{.ResourceKind}}.json"
+    echo "Ending test for ${TEST_CASE_NAME}"
 done
