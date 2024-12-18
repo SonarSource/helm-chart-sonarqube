fea1238ce0eba6075c78edea45c87f99d5c33afb
Carmine Vassallo <carmine.vassallo@sonarsource.com>
2024-12-18 15:40:25 +0100

    NO-JIRA fix missing tgz generation

--- .cirrus/package.sh
+++ .cirrus/package.sh
@@ -10,18 +10,14 @@ set -xeuo pipefail
 
 PREVIOUS_RELEASE=$(gh api "/repos/{owner}/{repo}/releases" --jq "[.[] | select(.target_commitish==\"${TARGET_BRANCH}\")][1].tag_name")
 
-# There MIGHT be a some edge case where PREVIOUS_RELEASE shouldn't be HEAD,
-# for example, releasing a patch for non-LTA. To be investigated.
-[[ -z "${PREVIOUS_RELEASE}" ]] && PREVIOUS_RELEASE="HEAD" || echo "${PREVIOUS_RELEASE}"
-
-CHARTS=$(ct list-changed --since "${PREVIOUS_RELEASE}" --target-branch "${TARGET_BRANCH}")
+[[ -z "${PREVIOUS_RELEASE}" ]] && CHARTS=("charts/sonarqube-dce" "charts/sonarqube") || CHARTS=$(ct list-changed --since "${PREVIOUS_RELEASE}" --target-branch "${TARGET_BRANCH}")
 
 BUILD_METADATA="-${BUILD_NUMBER}"
 [[ ${CIRRUS_RELEASE:-} != "" ]] && BUILD_METADATA=""
 
-echo "${CHARTS}"
+echo "${CHARTS[@]}"
 
-for chart in ${CHARTS}; do
+for chart in ${CHARTS[@]}; do
     _original_version=$(yq '.version' "${chart}"/Chart.yaml)
     _new_version="${_original_version}${BUILD_METADATA}"
     helm dependency build "${chart}"
