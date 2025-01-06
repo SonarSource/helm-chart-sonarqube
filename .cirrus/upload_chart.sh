9cc774621decd75236823369e6f4d89266bc5c60
Davi Koscianski Vidal <davi.koscianski-vidal@sonarsource.com>
2025-01-06 09:34:50 +0100

    SONAR-23970 Add conditions to CI

--- .cirrus/upload_chart.sh
+++ .cirrus/upload_chart.sh
@@ -2,19 +2,40 @@
 
 set -euo pipefail
 
-CURRENT_DIR=$(pwd)
-CHARTS=$(find $CIRRUS_WORKING_DIR -maxdepth 1 -name "*.tgz*" -type f -exec basename "{}" ";")
+: "${ARTIFACTORY_URL:?}"
+: "${ARTIFACTORY_ACCESS_TOKEN:?}"
+: "${CIRRUS_WORKING_DIR:?}"
+: "${CIRRUS_REPO_NAME:?}"
+: "${BUILD_NUMBER:?}"
 
-[[ "x$CHARTS" == "x" ]] && exit 0
+CHART_TO_UPLOAD=${1:-}
+
+# If there is a $1 argument, treat it as the chart to sign by looking for $1*.tgz* files
+# Otherwise, look for all *.tgz* files in the working directory
+NAME_GLOB="*.tgz*"
+if [[ -n "${CHART_TO_UPLOAD}" ]]; then
+    NAME_GLOB="${CHART_TO_UPLOAD}-[0-9]*.tgz*"
+fi
+
+find_charts=$(find "${CIRRUS_WORKING_DIR}" -maxdepth 1 -name "${NAME_GLOB}" -type f -exec basename "{}" ";" || exit 1)
+
+CHART_TO_UPLOAD=()
+while IFS= read -r chart; do
+    CHART_TO_UPLOAD+=("${chart}")
+done <<< "${find_charts}"
+
+if [[ ${#CHART_TO_UPLOAD[@]} -eq 0 ]]; then
+    echo "No charts found to upload."
+    exit 1
+fi
 
 jfrog config add repox \
-  --artifactory-url "${ARTIFACTORY_URL}" \
-  --access-token "${ARTIFACTORY_ACCESS_TOKEN}"
+    --artifactory-url "${ARTIFACTORY_URL}" \
+    --access-token "${ARTIFACTORY_ACCESS_TOKEN}"
 
-for chart in $CHARTS; do
-    cd $CIRRUS_WORKING_DIR
-    jfrog rt upload --build-name "$CIRRUS_REPO_NAME" --build-number "$BUILD_NUMBER" "$chart" sonarsource-helm-builds
-    cd $CURRENT_DIR
+for chart in "${CHART_TO_UPLOAD[@]}"; do
+    echo "Uploading ${chart}"
+    jfrog rt upload --build-name "${CIRRUS_REPO_NAME}" --build-number "${BUILD_NUMBER}" "${chart}" sonarsource-helm-builds
 done
 
-jfrog rt build-publish "$CIRRUS_REPO_NAME" "$BUILD_NUMBER"
+jfrog rt build-publish "${CIRRUS_REPO_NAME}" "${BUILD_NUMBER}"
