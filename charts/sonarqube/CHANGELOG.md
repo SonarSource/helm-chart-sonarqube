60dbe7e59e350245b9f5c5622dd7079e4cf92559
Jérémy Cotineau <jeremy.cotineau@sonarsource.com>
2025-03-26 15:28:44 +0100

    REL-3640 Release SonarQube Server 2025.2.0

--- charts/sonarqube/CHANGELOG.md
+++ charts/sonarqube/CHANGELOG.md
@@ -4,6 +4,7 @@ All changes to this chart will be documented in this file.
 ## [2025.2.0]
 * Update Chart's version to 2025.2.0
 * Update ingress-nginx subchart to 4.12.1
+* Upgrade SonarQube Server to 2025.2.0
 
 ## [2025.1.0]
 * Update Chart's version to 2025.1.0
--- tests/unit-test/schema_test.go
+++ tests/unit-test/schema_test.go
@@ -130,7 +130,7 @@ func TestShouldUseImageTag(t *testing.T) {
 	var renderedTemplate appsv1.StatefulSet
 	helm.UnmarshalK8SYaml(t, output, &renderedTemplate)
 
-	expectedContainerImage := "sonarqube:2025.1.0-enterprise"
+	expectedContainerImage := "sonarqube:2025.2.0-enterprise"
 	actualContainers := renderedTemplate.Spec.Template.Spec.Containers
 	assert.Equal(t, 1, len(actualContainers))
 	assert.Equal(t, expectedContainerImage, actualContainers[0].Image)
@@ -216,7 +216,7 @@ func TestDeveloperEdition(t *testing.T) {
 	var renderedTemplate appsv1.StatefulSet
 	helm.UnmarshalK8SYaml(t, output, &renderedTemplate)
 
-	expectedContainerImage := "sonarqube:2025.1.0-developer"
+	expectedContainerImage := "sonarqube:2025.2.0-developer"
 	actualContainers := renderedTemplate.Spec.Template.Spec.Containers
 	assert.Equal(t, 1, len(actualContainers))
 	assert.Equal(t, expectedContainerImage, actualContainers[0].Image)
