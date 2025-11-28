4adcc71c5916ce1baabd8e23a24b37e01ab20623
Carmine Vassallo <carmine.vassallo@sonarsource.com>
2025-11-28 12:54:37 +0100

    Fix security hotspot

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -13,7 +13,7 @@ BASE_FOLDER="${BASE_FOLDER:-"/root/.gcp/cache"}"
 
 mkdir -p ${BASE_FOLDER}
 
-curl -LO https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-${GCLOUD_CLI_VERSION}-linux-x86_64.tar.gz
+curl --proto "=https" --fail -LO  https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-${GCLOUD_CLI_VERSION}-linux-x86_64.tar.gz
 echo "${GCLOUD_CLI_CHECKSUM_NO_RENOVATE}  google-cloud-cli-${GCLOUD_CLI_VERSION}-linux-x86_64.tar.gz" | sha256sum -c
 tar -xf google-cloud-cli-${GCLOUD_CLI_VERSION}-linux-x86_64.tar.gz
 chmod +x ./google-cloud-sdk
@@ -22,7 +22,7 @@ rm -rf google-cloud-cli-${GCLOUD_CLI_VERSION}-linux-x86_64.tar.gz
 
 gcloud components install gke-gcloud-auth-plugin kubectl
 
-curl -LO https://github.com/GoogleCloudPlatform/marketplace-k8s-app-tools/archive/refs/tags/${MPDEV_VERSION}.tar.gz
+curl --proto "=https" --fail -LO https://github.com/GoogleCloudPlatform/marketplace-k8s-app-tools/archive/refs/tags/${MPDEV_VERSION}.tar.gz
 echo "${MPDEV_CHECKSUM_NO_RENOVATE}  ${MPDEV_VERSION}.tar.gz" | sha256sum -c
 tar -xf ${MPDEV_VERSION}.tar.gz
 mv marketplace-k8s-app-tools-${MPDEV_VERSION}/scripts/dev ${BASE_FOLDER}/mpdev
