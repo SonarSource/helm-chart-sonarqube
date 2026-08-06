4cfd6a4360a582d5ef1e7a06eccf1ea2a6236b06
Navya Teja Otturu <navya.otturu@sonarsource.com>
2026-08-06 11:13:31 +0200

    REL-4839 Release SonarQube Community Build 26.8

--- tests/unit-test/sonarqube_schema_test.go
+++ tests/unit-test/sonarqube_schema_test.go
@@ -15,7 +15,7 @@ import (
 
 var chartPath string = "../../charts/sonarqube"
 var releaseName string = "sonarqube"
-var expectedContainerImage string = "sonarqube:26.7.0.124771"
+var expectedContainerImage string = "sonarqube:26.8.0.126808"
 var sqStsTemplate []string = []string{"templates/sonarqube-sts.yaml"}
 
 func newSQHelmOptions() *helm.Options {
